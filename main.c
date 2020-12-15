46ee08818f88d063c0fb62b319f19c5d16da2c3a
Goodywon <goodywon@mutinies.net>
2020-12-15 15:09:39 -0800

    did strlen

--- main.c
+++ main.c
@@ -6,8 +6,21 @@
 /*   By: agoodwin <agoodwin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/15 14:24:27 by agoodwin          #+#    #+#             */
-/*   Updated: 2020/12/15 14:24:41 by agoodwin         ###   ########.fr       */
+/*   Updated: 2020/12/15 15:09:09 by agoodwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strlen(char const *str);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int ft_strlen(char const *str);
+
+int main()
+{
+    char* s = "Hello World";
+
+    printf("Mine: %d\n", ft_strlen(s));
+    printf("Them: %d\n", (int)strlen(s));
+}
